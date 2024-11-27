f131b0755b26fa0252342f8be13deabb6b71e1cb
thi-coelho <thiago.alvescoelho93@gmail.com>
2024-11-27 16:28:39 +0000

    added a simple print

--- src/git_2024287/Git_2024287.java
+++ src/git_2024287/Git_2024287.java
@@ -15,6 +15,8 @@ public class Git_2024287 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Thiago Alves Coelho - 2024287");
+        
     }
     
 }
